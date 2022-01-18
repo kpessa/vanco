6eb712dc102c5d7ef6688808013c1f710879630e
Kurt Pessa <kpessa@gmail.com>
2022-01-18 13:19:29 -0500

    worked on class based state, included Vd and Ke, went to two columns. Things are looking good.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from "react"
 import "../styles/index.css"
 
+import { Patient } from "../utils"
+
+import Context from "../components/Context"
 import Weight from "../components/Weight"
 import SCr from "../components/SCr"
 import CrCl from "../components/CrCl"
 import CrClCalculated from "../components/CrClCalculated"
 import Vd from "../components/Vd"
+import Ke from "../components/Ke"
 
 export default function Home() {
-  const initialPt = {
-    age: "",
-    height: "",
-    weight: "",
-    scr: "",
-    ibw: "",
-    bmi: "",
-    tbw_ibw: "",
-    manual_crcl: "",
-    crcl_calculated: "",
-  }
+  // const initialPt = {
+  //   age: "",
+  //   height: "",
+  //   weight: "",
+  //   scr: "",
+  //   ibw: "",
+  //   bmi: "",
+  //   tbw_ibw: "",
+  //   manual_crcl: "",
+  //   crcl_calculated: "",
+  // }
 
-  const [pt, setPt] = useState(initialPt)
+  const [pt, setPt] = useState(new Patient({}))
 
   useEffect(() => {
     console.log(pt)
@@ -56,11 +60,19 @@ export default function Home() {
 
   return (
     <div className="container">
+      <Context pt={pt} />
       <h1>Vanco AUC Calculator</h1>
-      <Weight pt={pt} setPt={setPt} />
-      {reorder()}
-      {displayManualCrClLogic()}
-      {pt.weight && (pt.calculated_crcl || pt.manual_crcl) && <Vd />}
+      <div style={{ display: "flex", gap: "4px" }}>
+        <div>
+          <Weight pt={pt} setPt={setPt} />
+          {reorder()}
+          {displayManualCrClLogic()}
+        </div>
+        <div>
+          {pt.weight && pt.crcl && <Vd pt={pt} setPt={setPt} />}
+          {pt.crcl && <Ke pt={pt} setPt={setPt} />}
+        </div>
+      </div>
     </div>
   )
 }
